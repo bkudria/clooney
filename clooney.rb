721e861e73c54646d827613e2fd015fd679ca9d1
Benjamin Kudria <ben@kudria.net>
2009-11-27 21:37:02 -0800

    rejigger app to play nicer with sinatra, rack.

--- clooney.rb
+++ clooney.rb
@@ -1,8 +1,9 @@
 require 'rubygems'
-require 'sinatra/base'
+require 'sinatra'
 
 require 'activesupport'
 require 'haml'
+require 'sass'
 require 'typhoeus'
 require 'json'
 
@@ -12,90 +13,84 @@ class Movie < Struct.new(:name, :character, :release_date, :fandango, :trailerad
   end
 end
 
-class Clooney < Sinatra::Base
+MQLREAD_BASE_URI = 'http://api.freebase.com/api/service/mqlread'
+FREEBASE_HEADERS = {'X-Requested-With' => 'Secret App 1.0'}
 
+CURRENT_RANGE  = [3.months.ago.to_date, Date.today]
+UPCOMING_RANGE = [Date.today, 5.years.from_now.to_date]
+
+configure do
+  enable  :logging
+  enable  :static
   set :haml, {:format => :html5}
   set :sass, {:style => :compact}
+end
 
-  MQLREAD_BASE_URI = 'http://api.freebase.com/api/service/mqlread'
-  FREEBASE_HEADERS = {'X-Requested-With' => 'Secret App 1.0'}
+configure :development do
+  FREEBASE_HEADERS = {'X-Requested-With' => 'Secret App 1.0', 'Cache-Control' => "no-cache"}
+end
 
-  CURRENT_RANGE  = [3.months.ago.to_date, Date.today]
-  UPCOMING_RANGE = [Date.today, 5.years.from_now.to_date]
+configure :production do
+  disable :logging
+  set :port, 80
 
-  configure do
-    enable  :logging
-    enable  :static
-    disable :run
-    set :public, File.join(File.dirname(__FILE__), "public")
+  use Rack::Auth::Basic do |username, password|
+    [username, password] == ['admin', 'max']
+  end
+end
+
+helpers do
+  def gen_mql_query(from, to)
+    JSON[File.read('clooney_movies.mql') % [from.to_date, to.to_date]]
   end
 
-  configure :development do
-    FREEBASE_HEADERS = {'X-Requested-With' => 'Secret App 1.0', 'Cache-Control' => "no-cache"}
+  def query_freebase(query)
+    query_uri = "#{MQLREAD_BASE_URI}?query=#{{:query => query}.to_json}"
+    response = Typhoeus::Request.get(
+                                     query_uri,
+                                     :headers => FREEBASE_HEADERS,
+                                     :timeout => 5000,      # 5 Seconds
+                                     :cache_timeout => 3600 # 1 Hour
+                                     )
+    return response.code == 200 ? response.body : nil
   end
 
-  configure :production do
-    disable :logging
-    set :port, 80
-
-    use Rack::Auth::Basic do |username, password|
-      [username, password] == ['admin', 'max']
-    end
-  end
-
-  helpers do
-    def gen_mql_query(from, to)
-      JSON[File.read('clooney_movies.mql') % [from.to_date, to.to_date]]
-    end
-
-    def query_freebase(query)
-      query_uri = "#{MQLREAD_BASE_URI}?query=#{{:query => query}.to_json}"
-      response = Typhoeus::Request.get(
-                                   query_uri, 
-                                   :headers => FREEBASE_HEADERS,
-                                   :timeout => 5000,      # 5 Seconds
-                                   :cache_timeout => 3600 # 1 Hour
-                                  )
-      return response.code == 200 ? response.body : nil
-    end
-
-    def extract_movies response_body
-      return nil if response_body.nil?
-      movies = JSON[response_body]["result"].map do |json|
-        m = Movie.new
-        m.name          = json["film"]["name"]
-        m.character     = json["character"]["name"] if json["character"]
-        m.release_date  = json["film"]["initial_release_date"]
-        [:imdb, :fandango, :traileraddict, :metacritic].each do |key|
-          mql_key = "#{key}:key"
-          m[key] = json["film"][mql_key]["value"] if json["film"][mql_key] and json["film"][mql_key]["value"]
-        end
-        m
+  def extract_movies response_body
+    return nil if response_body.nil?
+    movies = JSON[response_body]["result"].map do |json|
+      m = Movie.new
+      m.name          = json["film"]["name"]
+      m.character     = json["character"]["name"] if json["character"]
+      m.release_date  = json["film"]["initial_release_date"]
+      [:imdb, :fandango, :traileraddict, :metacritic].each do |key|
+        mql_key = "#{key}:key"
+        m[key] = json["film"][mql_key]["value"] if json["film"][mql_key] and json["film"][mql_key]["value"]
       end
+      m
     end
   end
+end
 
-  get '/' do
-    @title = "How many movies is George Clooney in right now?"
-    @current_movies  = extract_movies(query_freebase(gen_mql_query(*CURRENT_RANGE)))
-    @upcoming_movies = extract_movies(query_freebase(gen_mql_query(*UPCOMING_RANGE)))
-    if @current_movies.nil?
-      haml :error
-    else
-      haml :index
-    end
-  end
-
-  get '/style.css' do
-    content_type 'text/css', :charset => 'utf-8'
-    sass :style
-  end
-
-  not_found do
-    haml :notfound
-  end
-
-  error do
+get '/' do
+  @title = "How many movies is George Clooney in right now?"
+  @current_movies  = extract_movies(query_freebase(gen_mql_query(*CURRENT_RANGE)))
+  @upcoming_movies = extract_movies(query_freebase(gen_mql_query(*UPCOMING_RANGE)))
+  if @current_movies.nil?
     haml :error
+  else
+    haml :index
   end
+end
+
+get '/style.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :style
+end
+
+not_found do
+  haml :notfound
+end
+
+error do
+  haml :error
 end
--- config.ru
+++ config.ru
@@ -1,1 +1,2 @@
 require 'clooney'
+run Sinatra::Application
